79adf54ce1f7cbc29e8f36ef2ac0a4c2991d97cc
Devarshi <devarshi.s.patel@gmail.com>
2025-11-17 08:06:51 -0800

    Initial Commit

---
+++ src/config.ts
@@ -0,0 +1,78 @@
+export const siteConfig = {
+  name: "Devarshi Patel",
+  title: "Flutter Developer",
+  description: "Portfolio website of Devarshi Patel",
+  accentColor: "#027DFD",
+  social: {
+    email: "devarshi.s.patel@gmail.com",
+    linkedin: "https://www.linkedin.com/in/devarshi-patel-3a860a1b1/",
+    github: "https://github.com/devarshi-p",
+  },
+  aboutMe:
+    "I am a Flutter developer with 2 years of experience building high-performance, scalable mobile applications using Flutter, BLoC/Provider, and clean architecture. I specialize in integrating REST/GraphQL APIs, WebRTC, and LiveKit, and have hands-on expertise in creating complex UIs and native functionalities. I also have strong deployment experience on both iOS and Android, including real-time communication features.",
+  skills: ["Flutter", "Android Studio", "Xcode", "BLoC", "Provider", "REST API Integration", "GraphQL Integration", "WebRTC and LiveKit", "Native APIs and Permissions", "Git", "Firebase"],
+  projects: [
+    {
+      name: "Abby AI",
+      description:
+        "Abby AI is a cutting-edge app designed to empower entrepreneurs in the network marketing space. By combining interactive AI-driven training, behavior gamification, and personalized guidance, Abby helps sellers, leaders, and brands accelerate growth, boost retention, and build stronger teams.",
+      link: "https://www.abby-ai.com/",
+      skills: ["Flutter", "GraphQL", "Provider", "WebRTC and LiveKit", "REST APIs", "Git", "Firebase"],
+    },
+    {
+      name: "Appizap",
+      description:
+        "Appizap is an AI-driven no-code / low-code platform that empowers anyone—from non-technical founders to SMEs—to build production-ready web and mobile apps effortlessly. With intuitive drag-and-drop tools, natural-language prompting, a built-in database & workflow builder, and one-click native iOS/Android export, Appizap accelerates app creation and scalability without writing a single line of code.",
+      link: "https://play.google.com/store/apps/details?id=com.app.appizap",
+      skills: ["Flutter", "Provider", "Native APIs and Permissions"],
+    },
+    {
+      name: "Classter",
+      description:
+        "Classter is an all-in-one education management platform that unifies SIS, LMS, CRM, and ERP functionalities in a single app. It helps students, teachers, and administrators manage academics, communication, and daily operations seamlessly on both iOS and Android.",
+      link: "https://apps.apple.com/in/app/classter/id1485845874",
+      skills: ["Flutter", "REST APIs", "BLoC", "Git", "iOs"],
+    },
+  ],
+  experience: [
+    {
+      company: "DRC Systems India Limited",
+      title: "Associate Software Engineer",
+      dateRange: "Jul 2024 - Present",
+      bullets: [
+       "Contribute to Android and Flutter mobile app development to enhance product features.",
+       "Collaborate with cross-functional teams on scalable architecture and clean code practices.",
+       "Implement and maintain REST APIs and integrate third-party services for robust mobile functionality.",
+       "Support continuous improvement through code reviews, testing, and performance optimizations."
+      ],
+    },
+    {
+      company: "DRC Systems India Limited",
+      title: "Intern",
+      dateRange: "Jan 2024 - Jun 2024",
+      bullets: [
+       "Trained in coding best practices, clean architecture, SOLID principles, and version control.",
+       "Gained hands-on experience in Native Android development and building scalable app components.",
+       "Assisted in team projects, contributing to app features and learning collaborative development workflows."
+      ],
+    },
+  ],
+  education: [
+    {
+      school: "Dharmsinh Desai University",
+      degree: "Bachelor of Technology in Information Technology",
+      dateRange: "2021 - 2024",
+      achievements: [
+        "Graduated BTech with 8.1 grade",
+      ],
+    },
+    {
+      school: "Government Polytechnic Gandhinagar",
+      degree: "Diploma in Computer Engineering",
+      dateRange: "2018 - 2021",
+      achievements: [
+        "Completed Diploma with 9.92 CGPA",
+      ],
+    },
+  ],
+};
