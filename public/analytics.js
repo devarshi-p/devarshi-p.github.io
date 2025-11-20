f0d604a36f522740dbe791df9983dc914944caca
Devarshi <devarshi.s.patel@gmail.com>
2025-11-20 10:58:29 -0800

    added google analytics

---
+++ public/analytics.js
@@ -0,0 +1,5 @@
+// Google Analytics helper (placed in /public to avoid Astro/TS checking)
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-6HZ5694SFQ');
